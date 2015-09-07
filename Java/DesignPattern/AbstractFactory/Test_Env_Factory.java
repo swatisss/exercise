2f270f2eef6d533d27278b38dda7496c580fbb50
blue-210 <j5hfca7pm@gmail.com>
2015-09-07 16:04:53 +0900

    アノテーション追加

--- Java/DesignPattern/AbstractFactory/Test_Env_Factory.java
+++ Java/DesignPattern/AbstractFactory/Test_Env_Factory.java
@@ -1,8 +1,10 @@
 public class Test_Env_Factory extends Env_Change_Factory{
+   @Override
    public FindQuestionsCommand createFindQuestions(){
       return new FindQuestions_Prod();
    }
 
+   @Override
    public SendAnswersCommand createSendAnswers(){
       return new SendAnswers_Prod();
    }
