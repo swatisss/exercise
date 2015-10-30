6e61854a9d00d9071506908f97e9cf2d30c499a6
blue-210 <j5hfca7pm@gmail.com>
2015-10-30 19:57:00 +0900

    授業成果物

--- Java/J2EE/setclasspath.bat
+++ Java/J2EE/setclasspath.bat
@@ -1,6 +1,3 @@
 @echo off
 
 set classpath=.;C:\GitHub\exercise\Java\J2EE\WEB-INF\lib\javax.servlet.jsp.jstl-1.2.1.jar;C:\GitHub\exercise\Java\J2EE\WEB-INF\lib\javax.servlet.jsp.jstl-api-1.2.1.jar;C:\GitHub\exercise\Java\J2EE\WEB-INF\lib\jsp-api.jar;C:\GitHub\exercise\Java\J2EE\WEB-INF\lib\servlet-api.jar
-
-@echo on
-javac -encoding utf-8 factory/*.java -d ../classes
