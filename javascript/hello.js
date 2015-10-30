68e01d228cdf370548cd5c54f67f344516b60ed9
blue-210 <blue210@MacBook-Air.local>
2015-10-31 06:28:17 +0900

    授業復習

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 window.onload = function(){
    document.getElementById('button1').onclick = function(){
       showImage(document.getElementById('btn').value);
@@ -10,3 +11,16 @@ function showImage(num){
    htmlstr += "<img src='/Users/koyama/Pictures/img"+num+".jpg'>";
    document.getElementById("imageArea").innerHTML = htmlstr;
 }
+=======
+
+// 即時関数を使う
+// クロージャで内部の変数にiを保持しておく
+for(var i = 0; i < 3; i++){
+   document.getElementById("rd_"+i).onclick = (function(cnt){
+      return function(){
+         alert(cnt);
+      };
+   })(i);
+}
+//保持されたiが012と表示される
+>>>>>>> Stashed changes
